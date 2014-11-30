2dce83e02daadebfbe92596fe0445a1ac4cfb243
Shangheng Wu <shangheng.w@gmail.com>
2014-11-30 06:16:53 -0600

    first commit

---
+++ rational/RationalTest.java
@@ -0,0 +1,63 @@
+import junit.framework.TestCase;
+
+public class RationalTest extends TestCase {
+
+    protected Rational HALF;
+
+    protected void setUp() {
+      HALF = new Rational( 1, 2 );
+    }
+
+    // Create new test
+    public RationalTest (String name) {
+        super(name);
+    }
+
+    public void testEquality() {
+        assertEquals(new Rational(1,3), new Rational(1,3));
+        assertEquals(new Rational(1,3), new Rational(2,6));
+        assertEquals(new Rational(3,3), new Rational(1,1));
+    }
+
+    // Test for nonequality
+    public void testNonEquality() {
+        assertFalse(new Rational(2,3).equals(new Rational(1,3)));
+    }
+
+    public void testAccessors() {
+    	assertEquals(new Rational(2,3).numerator(), 2);
+    	assertEquals(new Rational(2,3).denominator(), 3);
+    }
+
+    public void testRoot() {
+        Rational s = new Rational( 1, 4 );
+        Rational sRoot = null;
+        try {
+            sRoot = s.root();
+        } catch (IllegalArgumentToSquareRootException e) {
+            e.printStackTrace();
+        }
+		/*
+		System.out.print("sRoot: ");
+		System.out.println(sRoot);
+		
+		System.out.print("HALF: ");
+		System.out.println(HALF);
+		
+		System.out.print("HALF.plus: ");
+		System.out.println(HALF.plus( Rational.getTolerance() ));
+		
+		System.out.print("HALF.minus: ");
+		System.out.println(HALF.minus( Rational.getTolerance() ));
+		*/
+        assertTrue( sRoot.isLessThan( HALF.plus( Rational.getTolerance() ) ) 
+                        && HALF.minus( Rational.getTolerance() ).isLessThan( sRoot ) );
+    }
+
+    public static void main(String args[]) {
+        String[] testCaseName = 
+            { RationalTest.class.getName() };
+        // junit.swingui.TestRunner.main(testCaseName);
+        junit.textui.TestRunner.main(testCaseName);
+    }
+}
